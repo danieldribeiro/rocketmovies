c6411f0957014c5098f11af54001be922c8c7cb6
Daniel Ribeiro <daniel.dribeiro@hotmail.com>
2024-05-25 08:40:01 -0300

    Create project

---
+++ src/pages/SingIn/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles"
+
+export function SignIn(){
+  return(
+    <Container>
+      <h1>Sign In</h1>
+    </Container>
+  )
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#1C1B1E  ",
+
+    GRAY_700: '#3E3B47',
+
+    WHITE: "#F4EDE8",
+    TITLE: '#FF859B',
+
+    TEXT: "#CAC4CF",
+    PLACEHOLDER: '#948F99',
+  }
+}
